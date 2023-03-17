fd71b7561af471f5d2cf786dcdc2ac7f3be6901e
James <willseymour804@gmail.com>
2023-03-17 05:45:22 +0000

    reformatted with prettier

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Timestamp Microservice
 
-A freecodecamp backend api challenge project. 
+A freecodecamp backend api challenge project.
 
 GET
 /api
@@ -12,8 +12,8 @@ GET
 
 Returns a JSON response with unix and utc versions of a valid date parameter.
 
-
 Requirements:
+
 - A request to /api/:date with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
 - A request to /api/:date with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
 - A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
 // Missing :date returns current date
-app.get('/api', function(req, res) {
-	res.json({unix: new Date().getTime(), utc: new Date()});
-})
+app.get("/api", function (req, res) {
+  res.json({ unix: new Date().getTime(), utc: new Date() });
+});
 
+app.get("/api/:date", function (req, res) {
+  const numericDate = Number(req.params.date);
+  let date;
 
-app.get('/api/:date', function(req, res) {
-	const numericDate = Number(req.params.date)
-	let date;
-	
-	if (isNaN(numericDate)) {
-		date = new Date(req.params.date);
-	} else {
-		date = new Date(numericDate);
-	}
-	
-	if (date == 'Invalid Date')	{
-		res.json({ error: 'Invalid Date' })
-	} else {
-		res.json({unix: date.getTime(), utc: date.toUTCString()});
-	}
-	
-})
+  if (isNaN(numericDate)) {
+    date = new Date(req.params.date);
+  } else {
+    date = new Date(numericDate);
+  }
 
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
 
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
